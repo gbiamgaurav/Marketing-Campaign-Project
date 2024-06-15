23a733a94de427e9268374d09feda80494278271
gbiamgaurav <gaurav.bhattacharya10@gmail.com>
2024-06-15 08:10:28 +0530

    data validation completed

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 
 from Prediction_project import logger
 from Prediction_project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from Prediction_project.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
     logger.info(f"Stage {STAGE_NAME} Started")
     obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f"Stage {STAGE_NAME} Completed")
+except Exception as e:
+    raise e
+
+
+STAGE_NAME = "Data Validation Stage"
+try:
+    logger.info(f"Stage {STAGE_NAME} Started")
+    obj = DataValidationTrainingPipeline()
     obj.main()
     logger.info(f"Stage {STAGE_NAME} Completed")
 except Exception as e:
